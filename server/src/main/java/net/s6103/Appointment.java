8262308518ab1c5a07b56b26a93770a5ad8ce99c
NaughtyEric <jia_206551349@163.com>
2025-09-24 18:05:37 +0800

    Add facility name into class Appointment; Created Facility name class

--- server/src/main/java/net/s6103/Appointment.java
+++ server/src/main/java/net/s6103/Appointment.java
@@ -5,21 +5,24 @@ import java.time.*;
 public class Appointment {
     private final int clientId;
     private final int appointmentId;
+    private final String facilityName;
     private Instant beginTime;
     private int lastingSeconds;
 
-    public  Appointment(int clientId, int appointmentId,  Instant beginTime, int lastingSeconds) {
+    public Appointment(int clientId, int appointmentId, String facilityName, Instant beginTime, int lastingSeconds) {
         this.clientId = clientId;
         this.appointmentId = appointmentId;
+        this.facilityName = facilityName;
         this.beginTime = beginTime;
         this.lastingSeconds = lastingSeconds;
     }
 
-    public int getClientId() {return clientId;}
-    public int getAppointmentId() {return appointmentId;}
-    public Instant getBeginTime() {return beginTime;}
-    public Instant getEndTime() {return beginTime.plusSeconds(lastingSeconds);}
-    public int getLastingSeconds() {return lastingSeconds;}
+    public final int getClientId() {return clientId;}
+    public final int getAppointmentId() {return appointmentId;}
+    public final Instant getBeginTime() {return beginTime;}
+    public final Instant getEndTime() {return beginTime.plusSeconds(lastingSeconds);}
+    public final String getFacility() {return facilityName;}
+    public final int getLastingSeconds() {return lastingSeconds;}
 
     /**
      * Shift the appointment `second` seconds later.
@@ -27,5 +30,9 @@ public class Appointment {
      */
     public void delay(int seconds) {
         beginTime = beginTime.plusSeconds(seconds);
+    }
+
+    public final String getFacilityName() {
+        return facilityName;
     }
 }
