eb740505b3f89457c22fd40245ed6e0d82b08c71
NaughtyEric <jia_206551349@163.com>
2025-10-03 01:36:05 +0800

    Create monitor class

---
+++ server/src/main/java/net/s6103/Monitor.java
@@ -0,0 +1,31 @@
+package net.s6103;
+
+import java.time.Instant;
+import java.time.Duration;
+
+/**
+ * Monitor of appointments.
+ */
+public class Monitor {
+    private final Instant beginInstant;
+    private final Duration monitorInterval;
+    private final ClientInfo client;
+
+    public Monitor(ClientInfo client, Duration monitorInterval) {
+        this.beginInstant = Instant.now();
+        // throw exception if longer than 7 days
+
+        this.monitorInterval = monitorInterval;
+        this.client = client;
+    }
+
+    public boolean isExpired() {
+        Instant end = beginInstant.plus(monitorInterval);
+        return Instant.now().isAfter(end);
+    }
+
+    public ClientInfo getClient() {
+        return client;
+    }
+}
+
