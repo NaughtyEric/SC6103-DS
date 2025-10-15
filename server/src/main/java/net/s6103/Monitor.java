d184272b56ed5f8f8cd91d332d01825ab515db36
NaughtyEric <jia_206551349@163.com>
2025-10-15 14:58:56 +0800

    Fake UTC time bug fix

--- server/src/main/java/net/s6103/Monitor.java
+++ server/src/main/java/net/s6103/Monitor.java
@@ -1,7 +1,6 @@
 package net.s6103;
 
-import java.time.Instant;
-import java.time.Duration;
+import java.time.*;
 
 /**
  * Monitor of appointments.
@@ -12,7 +11,13 @@ public class Monitor {
     private final ClientInfo client;
 
     public Monitor(ClientInfo client, Duration monitorInterval) {
-        this.beginInstant = Instant.now();
+        // 获取一个基于当前时间的UTC时间戳
+        ZoneId localZone = ZoneId.systemDefault(); // 比如 Asia/Singapore
+        ZonedDateTime localNow = ZonedDateTime.now(localZone);
+        LocalTime localTime = localNow.toLocalTime();
+        LocalDate localDate = localNow.toLocalDate();
+        ZonedDateTime utcSameClock = ZonedDateTime.of(localDate, localTime, ZoneOffset.UTC);
+        this.beginInstant = utcSameClock.toInstant();
         if (monitorInterval.toSeconds() > 60 * 60 * 24 * 7) {
             throw new IllegalArgumentException("Monitor interval too long");
         }
